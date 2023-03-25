a54c0f336ee8ef0598b0dc3979574d89df04de9c
Gederson <gedersongustavogbot@ufps.edu.co>
2023-03-25 10:34:17 -0500

    First project structure + Entity Employee

---
+++ src/main/java/com/junit/testing/Entity/EmployeeEntity.java
@@ -0,0 +1,28 @@
+package com.junit.testing.Entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+
+public class EmployeeEntity {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private String email;
+}
---
+++ src/main/java/com/junit/testing/Repository/EmployeeRepository.java
@@ -0,0 +1,4 @@
+package com.junit.testing.Repository;
+
+public class EmployeeRepository {
+}
